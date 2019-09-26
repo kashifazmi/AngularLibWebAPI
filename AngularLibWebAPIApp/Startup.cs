888dffc3809bff639b20643bed67e93c08f2b3f6
mohd.kashif <mohd.kashif.ext@siemens.com>
2019-09-26 16:20:08 +0530

    Fix cons iisue

--- AngularLibWebAPIApp/Startup.cs
+++ AngularLibWebAPIApp/Startup.cs
@@ -14,34 +14,46 @@ using Microsoft.Extensions.Options;
 namespace AngularLibWebAPIApp
 {
     public class Startup
+{
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseMvc();
-        }
+        Configuration = configuration;
     }
+
+    readonly string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+    public IConfiguration Configuration { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddCors(options =>
+        {
+            options.AddPolicy(MyAllowSpecificOrigins,
+            builder =>
+            {
+                builder.WithOrigins("http://localhost:4200",
+                                    "http://www.contoso.com");
+            });
+        });
+
+        services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+    }
+
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+        else
+        {
+            app.UseHsts();
+        }
+
+        app.UseCors(MyAllowSpecificOrigins); 
+
+        app.UseHttpsRedirection();
+        app.UseMvc();
+    }
+}
 }
---
+++ AngularLibWebAPIApp/Utility/ConnectionString.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AngularLibWebAPIApp.Utility
+{
+    public static class ConnectionString
+    {
+        //private static string cName = "Data Source=.; Initial Catalog=StudentManagement;User ID=sa;Password=123";
+        private static string cName = "server=localhost\\SQLEXPRESS;Database=EmployeeData;Trusted_Connection=True;";
+        public static string CName
+        {
+            get => cName;
+        }
+    }
+}
